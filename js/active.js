c8a6830b8c708af16afba0da086f7478643b0bd8
matsumoto.ai <matsumoto.ai.2288@members.co.jp>
2021-04-05 12:16:44 +0900

    fix: ナビスクロール 等修正

--- js/active.js
+++ js/active.js
@@ -2,6 +2,7 @@ function menu_toggle(){
         var button = document.getElementById('hamburger');
         var open = document.getElementsByClassName('hamburger_line');
         var navi = document.getElementById('navi');
+        var href = document.querySelectorAll('a[href^="#"]');
 
         button.addEventListener('click',function(){
             for(var i=0; i<open.length; i++){
@@ -10,7 +11,26 @@ function menu_toggle(){
             navi.classList.toggle('open');
         });
 
-        if(navi.classList.contains('open')==true){
-        
+        navi.addEventListener('click',function(){
+            for(var i=0; i<open.length; i++){
+                open[i].classList.remove('open');
+            }
+            navi.classList.remove('open');
+        });
+
+        for(var i=1; i<href.length; i++){
+            href[i].addEventListener('click',function(){
+                for(var i=0; i<open.length; i++){
+                    open[i].classList.remove('open');
+                }
+                navi.classList.remove('open');
+                menu_scroll();
+            });
         }
 }menu_toggle();
+
+function menu_scroll(){
+    var scroll = new SmoothScroll('a[href^="#"]',{
+        header: '#header'
+    });
+}
